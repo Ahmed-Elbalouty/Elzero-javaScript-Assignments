3154676adf90bf2523e34fe38def744d8cd40130
Ahmed Abdelsalam <ahmedelbalouty01@gmail.com>
2024-06-19 01:28:04 +0300

    Upload Assignments

---
+++ From Lesson 31 to 39/38to39.js
@@ -0,0 +1,118 @@
+// Assignment one from video 39
+let job = "Manager";
+let salary = 0;
+
+if (job === "Manager") {
+  salary = 8000;
+} else if (job === "IT" || job === "Support") {
+  salary = 6000;
+} else if (job === "Developer" || job === "Designer") {
+  salary = 7000;
+} else {
+  salary = 4000;
+}
+
+
+// Solution with switch
+switch (job) {
+  case "Manager":
+    salary = 8000;
+    console.log(salary);
+    break;
+  case "IT":
+  case "Support":
+    salary = 6000;
+    console.log(salary);
+    break;
+  case "Developer":
+  case "Designer":
+    salary = 7000;
+    console.log(salary);
+    break;
+  default:
+    salary = 4000;
+    console.log(salary);
+}
+// End of Assignment one from video 39
+
+
+// Assignment two from video 39
+let holidays = 0;
+let money = 0;
+
+switch (holidays) {
+  case 0:
+    money = 5000;
+    console.log(`My Money is ${money}`);
+    break;
+  case 1:
+  case 2:
+    money = 3000;
+    console.log(`My Money is ${money}`);
+    break;
+  case 3:
+    money = 2000;
+    console.log(`My Money is ${money}`);
+    break;
+  case 4:
+    money = 1000;
+    console.log(`My Money is ${money}`);
+    break;
+  case 5:
+    money = 0;
+    console.log(`My Money is ${money}`);
+    break;
+  default:
+    money = 0;
+    console.log(`My Money is ${money}`);
+}
+
+
+// Solution with If
+if (holidays === 0) {
+  money = 5000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 1 || holidays === 2) {
+  money = 3000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 3) {
+  money = 2000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 4) {
+  money = 1000;
+  console.log(`My Money is ${money}`);
+} else if (holidays === 5) {
+  money = 0;
+  console.log(`My Money is ${money}`);
+} else {
+  money = 0;
+  console.log(`My Money is ${money}`);
+}
+// End of Assignment two from video 39
+
+
+// Assignment 1 from website
+let day = "   friday  ";
+day = day.trim();
+day = day.slice(0, 1).toUpperCase() + day.slice(1);
+
+switch (day) {
+  case "Friday":
+  case "Saturday":
+  case "Sunday":
+    console.log("No Appointments Available");
+    break;
+  case "Monday":
+  case "Thursday":
+    console.log("From 10:00 AM To 5:00 PM");
+    break;
+  case "Tuesday":
+    console.log("From 10:00 AM To 6:00 PM");
+    break;
+  case "Wednesday":
+    console.log("From 10:00 AM To 7:00 PM");
+    break;
+  default:
+    console.log("Its Not A Valid Day");
+}
+// End of Assignment 1 from website
